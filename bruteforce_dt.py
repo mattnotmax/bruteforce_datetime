ed3fb0b25492973a9abc29466495a480a5a761c3
mattnotmax <39072319+mattnotmax@users.noreply.github.com>
2018-07-29 10:49:17 +1000

    Update

--- bruteforce_dt.py
+++ bruteforce_dt.py
@@ -4,7 +4,7 @@ script to brute-force binary data looking for valid dates and times.
 
 Currently supports Windows FILETIME
 
-usage: ./brutus.py <filename> <year to search in format 'YYYY'>
+usage: ./bruteforce_dt.py <filename> <year to search in format 'YYYY'>
 '''
 
 __version__ = '0.1'
@@ -20,7 +20,7 @@ def bruteforce_datetime():
         try:
             date_setting = int(sys.argv[2])
         except ValueError:
-            print('Error: check your command-line parameters. Should be ./brutus.py <filename> YYYY')
+            print('Error: check your command-line parameters. Should be ./bruteforce_dt.py <filename> YYYY')
             sys.exit()
         else:
             if date_setting >= 1601 and date_setting <= 2042:
@@ -48,12 +48,15 @@ def bruteforce_datetime():
         if bool(date_dic) == False:
             print('There were no identified records matching the year {}'.format(date_setting))
         else:
-            print('Possible Windows little-endian FILETIME records matching the year {} as follows:'.format(date_setting))
+            print('Windows little-endian FILETIME records (in UTC) matching the year {} as follows:'.format(date_setting))
             for key, value in sorted(date_dic.items()):
                 print('Offset: {:#x}\tDate: {}'.format(key, value))
-            print('\nNB: All records presented in UTC')
 
 def main():
+    print('bruteforce_dt.py: Test binary data for possible valid date/time formats. Version {}\n'.format(__version__))
+    if len(sys.argv) != 3:
+        print('Syntax: bruteforce_dt.py <FILENAME> <YYYY>')
+        sys.exit()
     bruteforce_datetime()
 
 if __name__== "__main__":
